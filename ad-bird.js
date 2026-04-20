a34bbd379b40f1612f722bb234126f47c8353b63
Stockout Studio <hello@stockout.studio>
2026-04-20 13:19:15 -0700

    Update: Increased minimum pipe height to 140px

--- ad-bird.js
+++ ad-bird.js
@@ -414,7 +414,7 @@ class AdBird {
     }
 
     _spawnPipe() {
-        const minPipeHeight = 120;
+        const minPipeHeight = 140;
         const h = Math.floor(Math.random() * (this.canvas.height - this.config.pipeGap - (minPipeHeight * 2))) + minPipeHeight;
         this.pipes.push({
             x: this.canvas.width, y: h, w: this.config.pipeWidth, gap: this.config.pipeGap,
